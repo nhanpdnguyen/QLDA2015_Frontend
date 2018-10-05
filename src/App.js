ad79ec2c29e68db4c89fe11e2c696c07c1a228e3
Dong-Nhan <ddnn97@hotmail.com>
2018-10-05 22:27:33 +0700

    Chia lại thư mục

--- src/App.js
+++ src/App.js
@@ -1,55 +1,27 @@
 import React, { Component } from 'react';
-import { Container, Row, Col } from 'reactstrap';
-import NavBar from './components/NavBar';
-import Carousel from './components/Carousel';
-import TitleBox from './components/TitleBox';
-import Footer from './components/Footer';
-import Header from './components/Header';
+import Home from './views/Home.js';
+import { BrowserRouter as Router, Route, Switch } from 'react-router-dom';
+
+const HelloWorld = function (props){
+	return <div>Hello world</div>
+}
 
 class App extends Component {
-    constructor(props) {
-        super(props);
+	constructor(props) {
+		super(props);
 
-    }
+	}
 
-    render() {
-        return (
-					<div>
-						<Header></Header>
-						<NavBar></NavBar>
-						<Container fluid>
-							<Row>
-								<Col lg="8" md="8" sm="8" xs="8">
-									<Row>
-										<Carousel></Carousel>
-									</Row>
-									<Row>
-										<Col lg="6" md="6" sm="6" xs="6">
-											<TitleBox header="Bài học" src="/images/Main-icon1-01.png">
-											</TitleBox>
-										</Col>
-										<Col lg="6" md="6" sm="6" xs="6">
-											<TitleBox header="Thi thử" src="/images/Main-icon2-01.png">
-											</TitleBox>
-										</Col>
-										<Col lg="6" md="6" sm="6" xs="6">
-											<TitleBox header="Trắc nghiệm" src="/images/Main-icon3-01.png">
-											</TitleBox>
-										</Col>
-										<Col lg="6" md="6" sm="6" xs="6">
-											<TitleBox header="Trò chơi" src="/images/Main-icon4-01.png">
-											</TitleBox>
-										</Col>
-									</Row>
-								</Col>
-								<Col lg="4" md="4" sm="4" xs="4">
-								</Col>
-							</Row>
-						</Container>
-						<Footer></Footer>
-					</div>
-        );
-    }
+	render() {
+		return (
+			<Router>
+				<Switch>
+					<Route exact path="/" component={Home} />
+					<Route path="/about" component={HelloWorld} />
+				</Switch>
+			</Router>
+		);
+	}
 }
 
 export default App;
---
+++ src/views/Home.js
@@ -0,0 +1,54 @@
+import React, { Component } from 'react';
+import { Container, Row, Col } from 'reactstrap';
+import NavBar from '../components/NavBar';
+import Carousel from '../components/Carousel';
+import TitleBox from '../components/TitleBox';
+import Footer from '../components/Footer';
+import Header from '../components/Header';
+
+class Home extends Component {
+	constructor(props) {
+		super(props);
+	}
+
+	render() {
+		return (
+			<div>
+				<Header></Header>
+				<NavBar></NavBar>
+				<Container fluid>
+					<Row>
+						<Col lg="8" md="8" sm="8" xs="8">
+							<Row>
+								<Carousel></Carousel>
+							</Row>
+							<Row>
+								<Col lg="6" md="6" sm="6" xs="6">
+									<TitleBox header="Bài học" src="/images/Main-icon1-01.png">
+									</TitleBox>
+								</Col>
+								<Col lg="6" md="6" sm="6" xs="6">
+									<TitleBox header="Thi thử" src="/images/Main-icon2-01.png">
+									</TitleBox>
+								</Col>
+								<Col lg="6" md="6" sm="6" xs="6">
+									<TitleBox header="Trắc nghiệm" src="/images/Main-icon3-01.png">
+									</TitleBox>
+								</Col>
+								<Col lg="6" md="6" sm="6" xs="6">
+									<TitleBox header="Trò chơi" src="/images/Main-icon4-01.png">
+									</TitleBox>
+								</Col>
+							</Row>
+						</Col>
+						<Col lg="4" md="4" sm="4" xs="4">
+						</Col>
+					</Row>
+				</Container>
+				<Footer></Footer>
+			</div>
+		);
+	}
+}
+
+export default Home;
