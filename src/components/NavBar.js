2a8f81507c54722fac7a37bcea7011d8a0f0e80c
herotf3 <thientf.97@gmail.com>
2018-10-11 23:19:45 +0700

    edit navbar to dashed border

--- src/components/NavBar.js
+++ src/components/NavBar.js
@@ -1,6 +1,6 @@
 import React, { Component } from 'react';
 import {
-	Collapse,
+  Collapse,
   Navbar,
   NavbarToggler,
   Nav,
@@ -14,7 +14,7 @@ import {
 import './NavBar.css';
 
 export default class NavBar extends Component {
-	constructor(props) {
+  constructor(props) {
     super(props);
 
     this.toggle = this.toggle.bind(this);
@@ -27,70 +27,70 @@ export default class NavBar extends Component {
       isOpen: !this.state.isOpen
     });
   }
-	render() {
-		return (
-		<div>
+  render() {
+    return (
+      <div>
         <Navbar light expand="sm">
           <NavbarToggler onClick={this.toggle} />
-          <Collapse isOpen={this.state.isOpen} navbar>
+          <Collapse isOpen={this.state.isOpen} navbar className="menu-content">
             <Nav className="mx-auto" navbar>
               <NavItem>
                 <NavLink href="/">
-								<img className="nav-item-image" alt="Trang chủ" src="/images/Nav-icon1-01.png"/>
-								<div className="nav-item-text ml-2 mt-2">Trang chủ</div>
-								</NavLink>
+                  <img className="nav-item-image" alt="Trang chủ" src="/images/Nav-icon1-01.png" />
+                  <div className="nav-item-text ml-2 mt-2">Trang chủ</div>
+                </NavLink>
               </NavItem>
 
-							<UncontrolledDropdown nav inNavbar>
+              <UncontrolledDropdown nav inNavbar>
                 <DropdownToggle nav caret>
-									<img className="nav-item-image" alt="Trang chủ" src="/images/Nav-icon2-01.png"/>
-									<div className="nav-item-text ml-2 mt-2">Bài học</div>
-								</DropdownToggle>
+                  <img className="nav-item-image" alt="Trang chủ" src="/images/Nav-icon2-01.png" />
+                  <div className="nav-item-text ml-2 mt-2">Bài học</div>
+                </DropdownToggle>
                 <DropdownMenu right>
                   <DropdownItem>Toán</DropdownItem>
                   <DropdownItem>Tiếng Việt</DropdownItem>
                 </DropdownMenu>
               </UncontrolledDropdown>
 
-							<UncontrolledDropdown nav inNavbar>
+              <UncontrolledDropdown nav inNavbar>
                 <DropdownToggle nav caret>
-									<img className="nav-item-image" alt="Trang chủ" src="/images/Nav-icon3-01.png"/>
-									<div className="nav-item-text ml-2 mt-2">Bài tập</div>
-								</DropdownToggle>
+                  <img className="nav-item-image" alt="Trang chủ" src="/images/Nav-icon3-01.png" />
+                  <div className="nav-item-text ml-2 mt-2">Bài tập</div>
+                </DropdownToggle>
                 <DropdownMenu right>
                   <DropdownItem>Toán</DropdownItem>
                   <DropdownItem>Tiếng Việt</DropdownItem>
                 </DropdownMenu>
               </UncontrolledDropdown>
-							
-							<UncontrolledDropdown nav inNavbar>
+
+              <UncontrolledDropdown nav inNavbar>
                 <DropdownToggle nav caret>
-									<img className="nav-item-image" alt="Trang chủ" src="/images/Nav-icon4-01.png"/>
-									<div className="nav-item-text ml-2 mt-2">Thi thử</div>
-								</DropdownToggle>
+                  <img className="nav-item-image" alt="Trang chủ" src="/images/Nav-icon4-01.png" />
+                  <div className="nav-item-text ml-2 mt-2">Thi thử</div>
+                </DropdownToggle>
                 <DropdownMenu right>
                   <DropdownItem>Toán</DropdownItem>
                   <DropdownItem>Tiếng Việt</DropdownItem>
                 </DropdownMenu>
               </UncontrolledDropdown>
-              
-							<NavItem>
+
+              <NavItem>
                 <NavLink href="/">
-									<img className="nav-item-image" alt="Trang chủ" src="/images/Nav-icon5-01.png"/>
-									<div className="nav-item-text ml-2 mt-2">Giới thiệu</div>
-								</NavLink>
+                  <img className="nav-item-image" alt="Trang chủ" src="/images/Nav-icon5-01.png" />
+                  <div className="nav-item-text ml-2 mt-2">Giới thiệu</div>
+                </NavLink>
               </NavItem>
-							<NavItem>
+              <NavItem>
                 <NavLink href="/">
-									<img className="nav-item-image" alt="Trang chủ" src="/images/Nav-icon6-01.png"/>
-									<div className="nav-item-text ml-2 mt-2">Liên hệ</div>
-								</NavLink>
+                  <img className="nav-item-image" alt="Trang chủ" src="/images/Nav-icon6-01.png" />
+                  <div className="nav-item-text ml-2 mt-2">Liên hệ</div>
+                </NavLink>
               </NavItem>
 
             </Nav>
           </Collapse>
         </Navbar>
       </div>
-		)
-	}
+    )
+  }
 }
