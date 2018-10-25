0a6bbace70ff646d80e2a554d782ad71f2debacf
MacBook <thientf.97@gmail.com>
2018-10-25 17:55:33 +0700

    Update Profile + Sign up page

--- src/App.js
+++ src/App.js
@@ -8,12 +8,23 @@ import NavBar from './components/NavBar';
 // views
 import Home from './views/Home.js';
 import DangNhap from './views/DangNhap';
+import DangKy from './views/DangKy'
+import UpdateProfile from './views/UpdateProfile'
 // css
 import './App.css';
 
 const HelloWorld = function (props) {
 	return <div>Hello world</div>
 }
+
+const cityOpt=[{value:"HCM",label:"tp.Hồ Chí Minh"},
+{value:"Hải Phòng",label:"Hải Phòng"},
+{value:"Ninh Thuận",label:"Ninh Thuận"},
+{value:"Nha Trang",label:"Nha Trang"},
+{value:"Cà Mau",label:"Cà Mau"},
+{value:"HN",label:"Hà Nội"}
+	]
+const rankOpt=[{value:"G",label:"Giỏi"},{value:"K",label:"Khá"},{value:"TB",label:"Trung Bình"},{value:"Y",label:"Yếu"}]
 
 class App extends Component {
 
@@ -28,6 +39,9 @@ class App extends Component {
 							<Switch>
 								<Route exact path="/" component={Home} />
 								<Route exact path="/dang-nhap" component={DangNhap} />
+								<Route exact path="/dang-ky" component={DangKy} />
+								<Route exact path="/update-profile" 
+									render={()=><UpdateProfile cityOpt={cityOpt} rankOpt={rankOpt}/>} />
 								<Route component={HelloWorld} />
 							</Switch>
 					</Container>
