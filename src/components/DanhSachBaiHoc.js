d2ce9175ac975c4986c41b641797e092aa86a09d
Dong-Nhan <ddnn97@hotmail.com>
2018-11-09 16:13:47 +0700

    update css DanhSachBaiHoc

--- src/components/DanhSachBaiHoc.js
+++ src/components/DanhSachBaiHoc.js
@@ -1,25 +1,25 @@
 import React, { Component } from 'react';
 import { Row, Col } from "reactstrap";
 
-import './BaiHoc.css';
+import './DanhSachBaiHoc.css';
 
-export default class BaiHoc extends Component {
+export default class DanhSachBaiHoc extends Component {
   render() {
     return (
       <Row className="flex-md-grow-1 align-self-start p-3">
-        <Col md="8" className="bai-hoc-container">
-          <Row className="align-items-center">
-            <Col xs="5" className="text-center">
+        <Col md="8" className="ds-bai-hoc-container">
+          <Row className="align-items-center ds-bai-hoc-title-container toan">
+            <Col xs="5" className="text-center ds-bai-hoc-title toan">
               Toán
             </Col>
             <Col xs="7">
               <img src="/images/Toan-icon-01.png" className="img-fluid" alt="Bài học Toán" />
             </Col>
           </Row>
-          <Row className="p-2">
+          <Row className="p-2 mt-3 ds-bai-hoc-content-container">
             <Col md="6" className="ds-bai-hoc-left-container">
               <Row>
-                <Col xs="12">Chương 1</Col>
+                <Col xs="12" className="ds-bai-hoc-chuyen-de-title toan">Chương 1</Col>
                 <Col>
                   <ul>
                     <li>Số thập phân</li>
@@ -28,7 +28,7 @@ export default class BaiHoc extends Component {
                 </Col>
               </Row>
               <Row>
-                <Col xs="12">Chương 2</Col>
+                <Col xs="12" className="ds-bai-hoc-chuyen-de-title toands-bai-hoc-chuyen-de-title toan">Chương 2</Col>
                 <Col>
                   <ul>
                     <li>Bốn phép tính về số thập phân</li>
@@ -39,7 +39,7 @@ export default class BaiHoc extends Component {
             </Col>
             <Col md="6" className="ds-bai-hoc-right-container">
               <Row>
-                <Col xs="12">Chương 3</Col>
+                <Col xs="12" className="ds-bai-hoc-chuyen-de-title toands-bai-hoc-chuyen-de-title toan">Chương 3</Col>
                 <Col>
                   <ul>
                     <li>Giải toán có lời văn</li>
@@ -48,7 +48,7 @@ export default class BaiHoc extends Component {
                 </Col>
               </Row>
               <Row>
-                <Col xs="12">Chương 4</Col>
+                <Col xs="12" className="ds-bai-hoc-chuyen-de-title toan">Chương 4</Col>
                 <Col>
                   <ul>
                     <li>Hình học</li>
