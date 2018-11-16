4d8c05da692c072c925e2435aaff7f10dba17710
Dong-Nhan <ddnn97@hotmail.com>
2018-11-16 22:24:24 +0700

    thêm game vào 1 iframe

--- src/components/TestGame.js
+++ src/components/TestGame.js
@@ -1,17 +1,32 @@
-import React, { Component } from 'react';
-import Unity, { UnityContent } from "react-unity-webgl";
+// import React, { Component } from 'react';
+// import Unity, { UnityContent } from "react-unity-webgl";
+
+// export default class TestGame extends Component {
+//   constructor(props) {
+//     super(props);
+
+//     this.unityContent = new UnityContent(
+//       "MyGame/BuildSprint3V2.json",
+//       "MyGame/UnityLoader.js",
+//     );
+//   }
+
+//   render() {
+//     return <Unity unityContent={this.unityContent} />;
+//   }
+// }
+
+import React, { Component } from 'react'
 
 export default class TestGame extends Component {
-  constructor(props) {
-    super(props);
-
-    this.unityContent = new UnityContent(
-      "MyGame/BuildSprint3V2.json",
-      "MyGame/UnityLoader.js",
-    );
-  }
-
   render() {
-    return <Unity unityContent={this.unityContent} />;
+    return (
+      <div style={{ "height": "100vh" }} className="row flex-fill">
+        <div className="col-12">
+          <iframe width="100%" height="100%" src="/MyGame" frameborder="0"></iframe>
+        </div>
+      </div>
+    )
   }
 }
+
