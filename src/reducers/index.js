acffd5a0a8a925a7a748088f5b5c7e0aab58fa64
Dong-Nhan <ddnn97@hotmail.com>
2018-12-09 16:54:12 +0700

    ráp API bảng vàng

--- src/reducers/index.js
+++ src/reducers/index.js
@@ -2,6 +2,7 @@ import auth from "./authReducer";
 import profile from './profileReducer';
 import learning from './learningReducer';
 import exercise from './exerciseReducer';
+import leaderBoard from './leaderBoardReducer';
 import { combineReducers } from "redux";
 import { REQUESTING } from "../actions/actionTypes";
 import messageReducer from './messageReducer.js'
@@ -14,5 +15,5 @@ const isRequesting = function (state = false, action) {
   }
 }
 
-const rootReducer = combineReducers({ isRequesting, auth, profile, learning, exercise, messageReducer});
+const rootReducer = combineReducers({ isRequesting, auth, profile, learning, exercise, messageReducer, leaderBoard});
 export default rootReducer;
