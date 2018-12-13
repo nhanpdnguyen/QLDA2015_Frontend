cde45007916babcaaa9b8bdcca475564c8bc770f
Phạm Tĩnh Hồng Tấn Tài <pthtantai97@gmail.com>
2018-12-13 15:22:45 +0700

    edit port chat service

--- src/config.js
+++ src/config.js
@@ -3,6 +3,6 @@ export default {
   LEARNING_API_BASE_URL: 'http://103.114.107.16:8000/api/learning',
   EXERCISE_API_BASE_URL: 'http://103.114.107.16:8000/api/exercise',
   EXAMINATION_API_BASE_URL: 'http://103.114.107.16:8000/api/examination',
-  CHAT_API_BASE_URL: 'http://127.0.0.1:8002/api/chat',
-  CHAT_SOCKET: '//127.0.0.1:8002'
+  CHAT_API_BASE_URL: 'http://103.114.107.16:8000/api/chat',
+  CHAT_SOCKET: '//103.114.107.16:8000'
 }
